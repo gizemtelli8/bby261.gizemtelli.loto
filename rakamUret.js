f7238f18db44d01ca7d0dc81b602311ddf3bb36b
gizemtelli8 <gizemtelli8@gmail.com>
2015-12-17 22:46:44 +0200

    sayısal loto

--- rakamUret.js
+++ rakamUret.js
@@ -1,19 +1,18 @@
 /*         BBY261          */
 /* Sayısal Loto Uygulaması */
-function yenile(){
 //Çekiliş yapılacak sayıların dizisi
 var rakamlar = new Array(49);
 
 //Oynanacak kolonun dizisi
 var loto = new Array(6);
 
-document.write('<center><img src="sayisalLotoLogo.jpg"></center>');
+document.write('<center><img src="sayisalLoto.jpg"></center>');
 
 //Rakam havuzunun oluşturulması
 for(var i=0; i<49; i++){
     rakamlar[i]=i+1;
 }
-document.write('<table cellpadding="5" cellspacing="10" border="0" align="center">');
+document.write('<table cellpadding="6" cellspacing="10" border="0" align="center">');
 
 //6 kolonun ekrana yazdırılması
 for(var i4=0; i4<6; i4++){
@@ -27,17 +26,17 @@ for(var i2=0; i2<6; i2++){
 }
 loto.sort(function(a, b){return a-b});
 
-
 //Tek kolonun yazdırılması
 for(var i3=0; i3<6; i3++){
-    document.write('<td align="right" background="top.png" >'+loto[i3]+'</td>');
+    document.write('<td width=18 heigth=18 background="top.jpg" id="yazitipi">'+loto[i3]+'</td>');
 }
 document.write('</tr>');
 }
 
 document.write('</table>');
 
-document.write('<center><img src="buton.png" onclick="yenile()" ></center>');
-    } 
-//fonksiyon sonu
-yenile();
+document.write('<p><center><img src="buton.jpg" onclick="yenile()" ></center></p>');
+
+function yenile(){
+    window.location.href="index.html";
+}
